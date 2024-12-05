c25a742a95ae60aec4bbc0a9c29d6d385cadd2e9
brittLiban <britt.liban@student.greenriver.edu>
2024-12-05 01:10:22 -0800

    adding the sql

---
+++ sql/admins.sql
@@ -0,0 +1,45 @@
+CREATE DATABASE pearup;
+
+USE pearup;
+DROP TABLE IF EXISTS jobapps;
+
+
+CREATE TABLE admins (
+	
+    username VARCHAR(255),
+    password VARCHAR(255)
+    
+);
+
+INSERT INTO admins (
+    username,
+    password
+)
+VALUES (
+    'Liban',
+    "1234"
+);
+
+SELECT * FROM adminsCREATE DATABASE pearup;
+
+USE pearup;
+DROP TABLE IF EXISTS jobapps;
+
+
+CREATE TABLE admins (
+	
+    username VARCHAR(255),
+    password VARCHAR(255)
+    
+);
+
+INSERT INTO admins (
+    username,
+    password
+)
+VALUES (
+    'Liban',
+    "1234"
+);
+
+SELECT * FROM admins
